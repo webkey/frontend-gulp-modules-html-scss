74870be0ecd3702aa7f1e2e4cd0f09ebadbe614e
s.ilchenko <s.ilchenko.imedia@gmail.com>
2021-03-08 23:09:50 +0300

    Update

--- tasks/templates.js
+++ tasks/templates.js
@@ -1,11 +1,19 @@
 import gulp from 'gulp';
+import plumber from 'gulp-plumber';
 import fileinclude from 'gulp-file-include';
+import htmlbeautify from 'gulp-html-beautify';
 
 gulp.task('templates', () => (
 	gulp.src('app/pages/*.html')
+		.pipe(plumber())
 		.pipe(fileinclude({
 			prefix: '@@',
-			basepath: '@file'
+			basepath: 'app' // '@file', '@root'
+		}))
+		.pipe(htmlbeautify({
+			"indent_with_tabs": true,
+			"indent_size": 4,
+			"max_preserve_newlines": 0
 		}))
 		.pipe(gulp.dest('dist/'))
 ));
